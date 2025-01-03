29a00eec95b747f33490848c656dc2f1a1fcbc40
Diogo Ferraz <57525336+Diogo-Ferraz@users.noreply.github.com>
2025-01-03 17:37:37 +0000

    chore: add database connection string and run first migration

--- TaskManagement.Auth/Data/Migrations/20250103173452_InitialCreate.Designer.cs
+++ TaskManagement.Auth/Data/Migrations/20250103173452_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskManagement.Auth.Data;
 
@@ -11,9 +12,11 @@ using TaskManagement.Auth.Data;
 namespace TaskManagement.Auth.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250103173452_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
